0cf905a7509a578a2589d4b76b2dbbc312d28fe0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 10:48:55 +0000

    Adding tarfile member sanitization to extractall()

--- gordo_components/serializer/serializer.py
+++ gordo_components/serializer/serializer.py
@@ -82,7 +82,26 @@ def loads(bytes_object: bytes) -> GordoBase:
         tarbuff.seek(0)
 
         with tarfile.open(fileobj=tarbuff, mode="r:gz") as archive:
-            archive.extractall(tmp)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, tmp)
         return load(os.path.join(tmp, "serialized_gordo_model"))
 
 
